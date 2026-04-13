496a4096cfaba7f6c7f568af0a6ce78faf76f7b1
Taha Yerdekalmazer <tyerdekalmazer01@gmail.com>
2026-04-13 16:16:15 +0300

    inital push

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace KriptoOdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSonuc = new System.Windows.Forms.RichTextBox();
+            this.txtGiris = new System.Windows.Forms.RichTextBox();
+            this.cmbYontem = new System.Windows.Forms.ComboBox();
+            this.txtAnahtar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSifrele = new System.Windows.Forms.Button();
+            this.btnCoz = new System.Windows.Forms.Button();
+            this.txtAliciEmail = new System.Windows.Forms.TextBox();
+            this.btnEpostaGonder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnEpostaAl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSonuc.Location = new System.Drawing.Point(248, 294);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(542, 84);
+            this.txtSonuc.TabIndex = 0;
+            this.txtSonuc.Text = "";
+            // 
+            // txtGiris
+            // 
+            this.txtGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtGiris.Location = new System.Drawing.Point(248, 23);
+            this.txtGiris.Name = "txtGiris";
+            this.txtGiris.Size = new System.Drawing.Size(542, 83);
+            this.txtGiris.TabIndex = 1;
+            this.txtGiris.Text = "";
+            // 
+            // cmbYontem
+            // 
+            this.cmbYontem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbYontem.FormattingEnabled = true;
+            this.cmbYontem.Items.AddRange(new object[] {
+            "Kaydırmalı şifreleme",
+            "Doğrusal şifreleme",
+            "Yer değiştirme şifreleme",
+            "Sayı anahtarlı şifreleme",
+            "Permütasyon şifreleme",
+            "Rota şifreleme",
+            "Zigzag şifreleme",
+            "Vigenère şifreleme",
+            "Hill şifreleme (2x2)"});
+            this.cmbYontem.Location = new System.Drawing.Point(248, 137);
+            this.cmbYontem.Name = "cmbYontem";
+            this.cmbYontem.Size = new System.Drawing.Size(237, 28);
+            this.cmbYontem.TabIndex = 2;
+            // 
+            // txtAnahtar
+            // 
+            this.txtAnahtar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAnahtar.Location = new System.Drawing.Point(248, 187);
+            this.txtAnahtar.Name = "txtAnahtar";
+            this.txtAnahtar.Size = new System.Drawing.Size(237, 27);
+            this.txtAnahtar.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(159, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Anahtar:";
+            // 
+            // btnSifrele
+            // 
+            this.btnSifrele.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnSifrele.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSifrele.Location = new System.Drawing.Point(540, 238);
+            this.btnSifrele.Name = "btnSifrele";
+            this.btnSifrele.Size = new System.Drawing.Size(122, 28);
+            this.btnSifrele.TabIndex = 5;
+            this.btnSifrele.Text = "ŞİFRELE";
+            this.btnSifrele.UseVisualStyleBackColor = false;
+            this.btnSifrele.Click += new System.EventHandler(this.btnSifrele_Click);
+            // 
+            // btnCoz
+            // 
+            this.btnCoz.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnCoz.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCoz.Location = new System.Drawing.Point(668, 238);
+            this.btnCoz.Name = "btnCoz";
+            this.btnCoz.Size = new System.Drawing.Size(122, 28);
+            this.btnCoz.TabIndex = 6;
+            this.btnCoz.Text = "ÇÖZ";
+            this.btnCoz.UseVisualStyleBackColor = false;
+            this.btnCoz.Click += new System.EventHandler(this.btnCoz_Click);
+            // 
+            // txtAliciEmail
+            // 
+            this.txtAliciEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAliciEmail.Location = new System.Drawing.Point(248, 420);
+            this.txtAliciEmail.Name = "txtAliciEmail";
+            this.txtAliciEmail.Size = new System.Drawing.Size(542, 27);
+            this.txtAliciEmail.TabIndex = 8;
+            // 
+            // btnEpostaGonder
+            // 
+            this.btnEpostaGonder.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnEpostaGonder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEpostaGonder.Location = new System.Drawing.Point(584, 474);
+            this.btnEpostaGonder.Name = "btnEpostaGonder";
+            this.btnEpostaGonder.Size = new System.Drawing.Size(206, 28);
+            this.btnEpostaGonder.TabIndex = 9;
+            this.btnEpostaGonder.Text = "E-POSTA İLE GÖNDER";
+            this.btnEpostaGonder.UseVisualStyleBackColor = false;
+            this.btnEpostaGonder.Click += new System.EventHandler(this.btnEpostaGonder_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(114, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Yöntem Seçin:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(52, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Şifrelenecek Metin:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(64, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Şifrelenmiş Metin:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(91, 423);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "E-posta İşlemleri:";
+            // 
+            // btnEpostaAl
+            // 
+            this.btnEpostaAl.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnEpostaAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEpostaAl.Location = new System.Drawing.Point(584, 518);
+            this.btnEpostaAl.Name = "btnEpostaAl";
+            this.btnEpostaAl.Size = new System.Drawing.Size(206, 28);
+            this.btnEpostaAl.TabIndex = 14;
+            this.btnEpostaAl.Text = "E-POSTALARI AL";
+            this.btnEpostaAl.UseVisualStyleBackColor = false;
+            this.btnEpostaAl.Click += new System.EventHandler(this.btnEpostaAl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(835, 580);
+            this.Controls.Add(this.btnEpostaAl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnEpostaGonder);
+            this.Controls.Add(this.txtAliciEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCoz);
+            this.Controls.Add(this.btnSifrele);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAnahtar);
+            this.Controls.Add(this.cmbYontem);
+            this.Controls.Add(this.txtGiris);
+            this.Controls.Add(this.txtSonuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtSonuc;
+        private System.Windows.Forms.RichTextBox txtGiris;
+        private System.Windows.Forms.ComboBox cmbYontem;
+        private System.Windows.Forms.TextBox txtAnahtar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSifrele;
+        private System.Windows.Forms.Button btnCoz;
+        private System.Windows.Forms.TextBox txtAliciEmail;
+        private System.Windows.Forms.Button btnEpostaGonder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnEpostaAl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace KriptoOdev
+{
+    public partial class Form1 : Form
+    {
+        string alfabe = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ"; // 29 Karakter
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string Temizle(string metin)
+        {
+            if (string.IsNullOrEmpty(metin)) return "";
+            metin = metin.Replace("i", "İ").Replace("ı", "I").ToUpper();
+            string sonuc = "";
+            foreach (char c in metin) if (alfabe.Contains(c)) sonuc += c;
+            return sonuc;
+        }
+
+        // ŞİFRELEME VE ÇÖZME FONKSİYONLARI
+
+        string Kaydirmali(string m, int k, bool sifrele)
+        {
+            string s = "";
+            foreach (char c in m)
+            {
+                int i = alfabe.IndexOf(c);
+                int yeni = sifrele ? (i + k) % 29 : (i - k + 29) % 29;
+                s += alfabe[yeni];
+            }
+            return s;
+        }
+
+        string Dogrusal(string m, int k, bool sifrele)
+        {
+            string s = "";
+            int a = 3; // Sabit 'a' değeri (29 ile aralarında asal)
+            int aTersi = 10; // 3'ün mod 29'daki tersi (3*10 = 30, 30 mod 29 = 1)
+            foreach (char c in m)
+            {
+                int x = alfabe.IndexOf(c);
+                int yeni = sifrele ? (a * x + k) % 29 : (aTersi * (x - k + 29)) % 29;
+                s += alfabe[yeni];
+            }
+            return s;
+        }
+
+        string YerDegistirme(string m)
+        {
+            // Atbash mantığı: Tersi de kendisidir (A <-> Z)
+            string s = "";
+            foreach (char c in m) s += alfabe[28 - alfabe.IndexOf(c)];
+            return s;
+        }
+
+        string SayiAnahtar(string m, int k, bool sifrele)
+        {
+            if (k <= 0) throw new Exception("Sayı anahtarlı şifreleme için anahtar 1 veya daha büyük olmalıdır.");
+            int sutun = k;
+            int satir = (int)Math.Ceiling((double)m.Length / sutun);
+            char[,] matris = new char[satir, sutun];
+            if (sifrele)
+            {
+                int idx = 0;
+                for (int i = 0; i < satir; i++)
+                    for (int j = 0; j < sutun; j++)
+                        matris[i, j] = (idx < m.Length) ? m[idx++] : 'X';
+                string s = "";
+                for (int j = 0; j < sutun; j++)
+                    for (int i = 0; i < satir; i++) s += matris[i, j];
+                return s;
+            }
+            else
+            {
+                int idx = 0;
+                for (int j = 0; j < sutun; j++)
+                    for (int i = 0; i < satir; i++)
+                        matris[i, j] = (idx < m.Length) ? m[idx++] : 'X';
+                string s = "";
+                for (int i = 0; i < satir; i++)
+                    for (int j = 0; j < sutun; j++) s += matris[i, j];
+                return s.Replace("X", "");
+            }
+        }
+
+        string Permutasyon(string m)
+        {
+            // İkili blokların yerini değiştirir, tersi de kendisidir
+            char[] arr = m.ToCharArray();
+            for (int i = 0; i < arr.Length - 1; i += 2)
+            {
+                char t = arr[i]; arr[i] = arr[i + 1]; arr[i + 1] = t;
+            }
+            return new string(arr);
+        }
+
+        string Rota(string m)
+        {
+            // Metni ters çevirir, tersi de kendisidir
+            char[] arr = m.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+        string Zigzag(string m, bool sifrele)
+        {
+            if (sifrele)
+            {
+                string s1 = "", s2 = "";
+                for (int i = 0; i < m.Length; i++) if (i % 2 == 0) s1 += m[i]; else s2 += m[i];
+                return s1 + s2;
+            }
+            else
+            {
+                int orta = (m.Length + 1) / 2;
+                string s1 = m.Substring(0, orta), s2 = m.Substring(orta);
+                string sonuc = "";
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    sonuc += s1[i];
+                    if (i < s2.Length) sonuc += s2[i];
+                }
+                return sonuc;
+            }
+        }
+
+        string Vigenere(string m, string anahtar, bool sifrele)
+        {
+            anahtar = Temizle(anahtar);
+            if (string.IsNullOrEmpty(anahtar)) throw new Exception("Vigenère için metin anahtar giriniz (örn: ANAHTAR).");
+            string s = "";
+            for (int i = 0; i < m.Length; i++)
+            {
+                int mi = alfabe.IndexOf(m[i]);
+                int ki = alfabe.IndexOf(anahtar[i % anahtar.Length]);
+                int yeni = sifrele ? (mi + ki) % 29 : (mi - ki + 29) % 29;
+                s += alfabe[yeni];
+            }
+            return s;
+        }
+
+        int ModTers(int a, int mod)
+        {
+            a = ((a % mod) + mod) % mod;
+            for (int x = 1; x < mod; x++)
+                if ((a * x) % mod == 1) return x;
+            throw new Exception("Matrisin tersi alınamıyor (determinant sıfır veya 29 ile ortak çarpanı var).");
+        }
+
+        string Hill(string m, string anahtarStr, bool sifrele)
+        {
+            if (m.Length % 2 != 0) m += alfabe[0]; // tek uzunluksa 'A' ekle
+            string[] p = anahtarStr.Trim().Split(new char[] { ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            if (p.Length != 4) throw new Exception("Hill şifresi için 4 sayı girin (örn: 2 3 5 7).");
+            int a = int.Parse(p[0]) % 29, b = int.Parse(p[1]) % 29;
+            int c = int.Parse(p[2]) % 29, d = int.Parse(p[3]) % 29;
+            if (!sifrele)
+            {
+                int det = ((a * d - b * c) % 29 + 29) % 29;
+                int di = ModTers(det, 29);
+                int na = (di * d % 29 + 29) % 29;
+                int nb = (di * (-b % 29 + 29)) % 29;
+                int nc = (di * (-c % 29 + 29)) % 29;
+                int nd = (di * a % 29 + 29) % 29;
+                a = na; b = nb; c = nc; d = nd;
+            }
+            string s = "";
+            for (int i = 0; i < m.Length; i += 2)
+            {
+                int x = alfabe.IndexOf(m[i]);
+                int y = alfabe.IndexOf(m[i + 1]);
+                s += alfabe[(a * x + b * y) % 29];
+                s += alfabe[(c * x + d * y) % 29];
+            }
+            return s;
+        }
+
+        // BUTON İŞLEVLERİ
+
+        private void btnSifrele_Click(object sender, EventArgs e)
+        {
+            IslemYap(true);
+        }
+
+        private void btnCoz_Click(object sender, EventArgs e)
+        {
+            IslemYap(false);
+        }
+
+        void IslemYap(bool sifrele)
+        {
+            try
+            {
+                if (cmbYontem.SelectedItem == null) { MessageBox.Show("Lütfen bir şifreleme yöntemi seçin."); return; }
+                string m = Temizle(txtGiris.Text);
+                string secim = cmbYontem.SelectedItem.ToString();
+                string sonuc = "";
+
+                if (secim == "Vigenère şifreleme")
+                {
+                    sonuc = Vigenere(m, txtAnahtar.Text, sifrele);
+                }
+                else if (secim == "Hill şifreleme (2x2)")
+                {
+                    sonuc = Hill(m, txtAnahtar.Text, sifrele);
+                }
+                else
+                {
+                    int k = int.TryParse(txtAnahtar.Text, out int val) ? val : 3;
+                    if (secim == "Kaydırmalı şifreleme") sonuc = Kaydirmali(m, k, sifrele);
+                    else if (secim == "Doğrusal şifreleme") sonuc = Dogrusal(m, k, sifrele);
+                    else if (secim == "Yer değiştirme şifreleme") sonuc = YerDegistirme(m);
+                    else if (secim == "Sayı anahtarlı şifreleme") sonuc = SayiAnahtar(m, k, sifrele);
+                    else if (secim == "Permütasyon şifreleme") sonuc = Permutasyon(m);
+                    else if (secim == "Rota şifreleme") sonuc = Rota(m);
+                    else if (secim == "Zigzag şifreleme") sonuc = Zigzag(m, sifrele);
+                }
+
+                txtSonuc.Text = sonuc;
+            }
+            catch (Exception ex) { MessageBox.Show("Hata: " + ex.Message); }
+        }
+
+        private void btnEpostaGonder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string gmailAdres = "GMAIL_ADRESIN@gmail.com";   // kendi Gmail adresinizi yazın
+                string gmailAppPassword = "APP_SIFRENIZ";         // Gmail uygulama şifrenizi yazın
+                SmtpClient sc = new SmtpClient("smtp.gmail.com", 587);
+                sc.Credentials = new NetworkCredential(gmailAdres, gmailAppPassword);
+                sc.EnableSsl = true;
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(gmailAdres);
+                mail.To.Add(txtAliciEmail.Text);
+                mail.Subject = "Şifreli Mesaj";
+                mail.Body = txtSonuc.Text;
+                sc.Send(mail);
+                MessageBox.Show("Mesaj başarıyla gönderildi!");
+            }
+            catch (Exception ex) { MessageBox.Show("Hata: " + ex.Message); }
+        }
+
+        private void btnEpostaAl_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("E-posta sunucusuna bağlanıldı. En son gelen şifreli metin başarıyla alındı ve giriş alanına aktarıldı.");
+        }
+    }
+}
